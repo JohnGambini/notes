8b3ef00161c4c5af139312a9aa90234744fc9000
john gambini <john.a.gambini@gmail.com>
2018-09-20 18:13:01 -0400

    Added set_siteNameMenu function to #mainmenuItem2 div with a different id for the menuTrigger button.

--- themes/calliopesutra-1.0/default_contentMenu.php
+++ themes/calliopesutra-1.0/default_contentMenu.php
@@ -12,11 +12,13 @@ $menuId = 'popupContentMenu';
 ?>
 <!-- calliope-sutra-1.0 default_contentMenu -->
 <div id="contentMenu" class="contentMenu">
-	<div id="mainmenuItem2">&nbsp;</div>
+	<div id="mainmenuItem2" style="margin:0em 0em 0em 20px">
+			<?php set_siteNameMenu($contentObj,$userObj,'contentMenuTrigger-2') ?>
+	</div>
 	<div id="mainmenuItem3">
 		<div id="pageTitle" class="fontSpecSmall" style="width:100%;margin:0em 0em 0em 0.5em;text-align:center"><?php set_pageTitle($contentObj) ?></div>
 	</div>
-	<div id="mainmenuItem4">
+	<div id="mainmenuItem4"  style="margin:0em 1% 0em 0em; align-items:center; text-align:right">
 		<?php set_siteUserMenu($contentObj, $userObj); set_articleEditLink($contentObj, $userObj) ?>
 	</div>
 </div>
