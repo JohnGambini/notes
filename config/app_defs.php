5d4780d16e3e74f6294dbdda921e9d20d33d5c23
john gambini <john.a.gambini@gmail.com>
2018-12-03 23:54:59 -0500

    ***

--- config/app_defs.php
+++ config/app_defs.php
@@ -6,12 +6,12 @@
  *
  ----------------------------------------------------------------------*/
 define('APP_NAME','Manîn A. Bocss');
-define('SITE_NAME', "http://localhost");
+define('SITE_NAME', 'http://' . $_SERVER['HTTP_HOST']);
 define('DATABASE', 'workbench_1');
 define('THEME','default');
 define('SUBSITE_NAME', '/notes');
-define('WORKBENCH_FOLDER', SITE_NAME . SUBSITE_NAME . '/vendor/workbench/core');
-//define('WORKBENCH_FOLDER', SITE_NAME . '/workbench');
+//define('WORKBENCH_FOLDER', SITE_NAME . SUBSITE_NAME . '/vendor/workbench/core');
+define('WORKBENCH_FOLDER', SITE_NAME . '/workbench');
 
 define('WEBAPP', SITE_NAME . SUBSITE_NAME);
 define( 'THEMEDIR', SUBSITE_NAME . '/themes');
@@ -22,8 +22,8 @@ define('PDFDIR', SITE_NAME . '/pdf');
 define('WEBROOT', $_SERVER["DOCUMENT_ROOT"]);
 define('SUBSITE_DIR', WEBROOT . "\\notes");
 define('SOURCEDIR', ABSDIR . "\\themes");
-define('WORKBENCH_DIR', SUBSITE_DIR . "\\vendor\\workbench\\core" );
-//define('WORKBENCH_DIR', WEBROOT . "\\workbench" );
+//define('WORKBENCH_DIR', SUBSITE_DIR . "\\vendor\\workbench\\core" );
+define('WORKBENCH_DIR', WEBROOT . "\\workbench" );
 
 //pdo connection information
 define('DB_HOST','127.0.0.1');
@@ -37,5 +37,5 @@ define('LOCALE_DIR', WORKBENCH_DIR .'/locale');
 define('DEFAULT_LOCALE', 'en_US');
 
 
-define('DEBUG_VERBOSE', true);
+define('DEBUG_VERBOSE', false);
 ?>
