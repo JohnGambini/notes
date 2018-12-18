72a5d01ff7db96a1fbb7e3388ca9d32e4bb6f777
john gambini <john.a.gambini@gmail.com>
2018-12-17 23:31:55 -0500

    profile sidebar needed siteName menu when contentMenu is present.

---
+++ themes/calliopesutra-1.0/profile_sidebar.php
@@ -0,0 +1,34 @@
+<?php
+/*--------------------------------------------------------------------------------------------
+* profile_sidebar.php
+*
+* Copyright 2015 2016 2017 2018 by John Gambini
+*
+---------------------------------------------------------------------------------------------*/
+global $dbObj;
+global $contentObj;
+global $sqlObject;
+global $dataArrays;
+global $userObj;
+
+?>
+<div id="sidebarContainer" class="sidebarContainer">
+<div id="sidebar" class="sidebar">
+<div style="margin:0.5em 0em 0em 0em">
+<?php set_siteNameMenu($contentObj,$userObj)?>
+<?php echo get_languagesString($contentObj,$dataArrays) ?>
+<?php set_articleEditSidebarWidget($dbObj, $contentObj, $userObj,$sqlObject, $dataArrays)?>
+	<div class="wb-dialog">
+	  <div style="margin:1em">
+		<img class="leftBarImage" style="border-radius:8px" src="<?php echo CONTENTDIR . $contentObj->ownerImage ?>"/>
+		<p/>
+		<?php echo $contentObj->ownerBio ?>
+	  </div>
+	</div>
+</div>
+</div> <!-- close sidebar -->
+</div> <!-- sidebar container -->
+
+
+
+
